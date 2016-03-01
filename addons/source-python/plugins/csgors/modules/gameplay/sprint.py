d096bb48231c20e6370340cf030090f0f6eff225
KirillMysnik <kirill@mysnik.com>
2016-03-01 18:42:47 +0300

    Added stepsounds Players now can't shoot while sprinting

--- addons/source-python/plugins/csgors/modules/gameplay/sprint.py
+++ addons/source-python/plugins/csgors/modules/gameplay/sprint.py
@@ -1,4 +1,11 @@
+from random import choice
+from time import time
+
+from engines.server import global_vars
 from engines.sound import Sound
+from entities.constants import INVALID_ENTITY_INTHANDLE
+from entities.entity import Entity
+from entities.helpers import index_from_inthandle
 from entities.hooks import EntityCondition, EntityPreHook
 from events import Event
 from listeners import OnTick
@@ -17,6 +24,15 @@ from .stamina import StaminaConsumers
 
 SPRINT_START_SOUND = Sound('player/suit_sprint.wav')
 LOW_STAMINA_SOUND = Sound('player/suit_denydevice.wav')
+STEP_SOUNDS = [
+    'npc/combine_soldier/gear1.wav',
+    'npc/combine_soldier/gear2.wav',
+    'npc/combine_soldier/gear3.wav',
+    'npc/combine_soldier/gear4.wav',
+    'npc/combine_soldier/gear5.wav',
+    'npc/combine_soldier/gear6.wav',
+]
+STEP_SOUND_INTERVAL = 0.3
 DEFAULT_PLAYER_SPEED = 0.6
 SPRINTING_PLAYER_SPEED = 1.75
 AIRBORNE_PLAYER_SPEED = 1
@@ -30,6 +46,11 @@ class SprintingPlayer:
         self.sprinting = False
         self.key_pressed = False
         self.speed = 0
+        self.last_step = 0
+
+        self._step_sounds = []
+        for sound_path in STEP_SOUNDS:
+            self._step_sounds.append(Sound(sound_path, player.index))
 
         player.gravity = DEFAULT_PLAYER_GRAVITY
 
@@ -37,6 +58,12 @@ class SprintingPlayer:
         if self.speed != speed:
             self.speed = speed
             self.player.speed = speed
+
+    def step(self):
+        cur_step = time()
+        if cur_step - self.last_step >= STEP_SOUND_INTERVAL:
+            choice(self._step_sounds).play()
+            self.last_step = cur_step
 
 
 class PlayerManager(dict):
@@ -79,7 +106,17 @@ def pre_player_run_command(stack_data):
     if usercmd.buttons & PlayerButtons.SPEED:
         if sprinting_player.key_pressed and sprinting_player.sprinting:
             if stamina_player.has_stamina_for(StaminaConsumers.SPRINT):
+
+                # Consume stamina
                 stamina_player.consume(StaminaConsumers.SPRINT)
+
+                # Cancel attacking
+                usercmd.buttons &= ~PlayerButtons.ATTACK
+                usercmd.buttons &= ~PlayerButtons.ATTACK2
+
+                # Step sound
+                sprinting_player.step()
+
             else:
                 sprinting_player.sprinting = False
                 LOW_STAMINA_SOUND.play(sprinting_player.player.index)
@@ -106,6 +143,15 @@ def listener_on_tick():
                 sprinting_player.ensure_speed(SPRINTING_PLAYER_SPEED)
             else:
                 sprinting_player.ensure_speed(DEFAULT_PLAYER_SPEED)
+
+        if sprinting_player.sprinting:
+            inthandle = sprinting_player.player.active_weapon
+            if inthandle == INVALID_ENTITY_INTHANDLE:
+                continue
+
+            entity = Entity(index_from_inthandle(inthandle))
+            entity.next_attack = global_vars.current_time + 1
+            entity.next_secondary_fire_attack = global_vars.current_time + 1
 
 
 @Event('player_spawn')
